8ac148f0ee0619574d93a8d9664c9d7872c36551
Zuzia Zuzewicz <zuzannazuzewicz@MacBook-Air-Zuzia.local>
2018-01-25 16:26:07 +0100

    Added component life cycle methods

--- script.js
+++ script.js
@@ -18,12 +18,28 @@ var Counter = React.createClass({
 		});
 	},
 
+	shouldComponentUpdate: function() {
+		console.log('To metoda, która sprawdza, czy komponent powinien zostać zaktualizowany');
+	},
+
+	componentWillMount: function() {
+		console.log('To jest metoda componentWillMount, używana zaraz przed render');
+	},
+
 	render: function() {
 		return React.createElement('div', {},
 			React.createElement('button', {onClick: this.decrement}, 'Odejmij'),
 			React.createElement('span', {}, 'Licznik ' + this.state.counter),
 			React.createElement('button', {onClick: this.increment}, 'Dodaj'),
 		);
+	},
+
+	componentDidMount: function() {
+		console.log('To metoda componentDidMount');
+	},
+
+	componentWillUnmount: function() {
+		console.log('Ta metoda zostanie wykorzystana, kiedy jakiś element należy odczepić od DOM');
 	}
 });
 
